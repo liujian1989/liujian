b16bd7214933cea8098777dd98bdaddde4dbcfa5
liujian1989 <785000418@qq.com>
2017-05-06 09:28:24 +0800

    master Signed-off-by: liujian1989 <785000418@qq.com>

---
+++ src/net/liujian/cheer/sample/ch09/TrigonometricFunction.java
@@ -0,0 +1,15 @@
+package net.liujian.cheer.sample.ch09;
+
+public class TrigonometricFunction {
+
+	public static void main(String[] args) {
+		System.out.println("90度的正弦值: "+Math.sin(Math.PI/2));
+		System.out.println("0度的余弦值: "+Math.cos(0));
+		System.out.println("60度的正切值: "+Math.tan(Math.PI/3));
+		System.out.println("2的平方根与2商的反余弦值: "+Math.acos(Math.sqrt(2)/2));
+		System.out.println("120度的弧度值: "+Math.toRadians(120.0));
+		System.out.println("π/2的角度值: "+Math.toDegrees(Math.PI/2));
+		
+	}
+
+}
