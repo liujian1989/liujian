532dd4ae61aa6f3eaac5b79c9769ba3336fb752d
liujian1989 <785000418@qq.com>
2017-05-11 23:36:34 +0800

    master Signed-off-by: liujian1989 <785000418@qq.com>

---
+++ src/net/liujian/cheer/LearnStudy/mianxiangduixiang/Chongwu.java
@@ -0,0 +1,17 @@
+package net.liujian.cheer.LearnStudy.mianxiangduixiang;
+
+public class Chongwu {
+static String name;
+String address;
+	public static void main(String[] args) {
+		Chongwu.name="丘比特";
+				Chongwu c1=new Chongwu();
+				c1.address="东莞动物园";
+			    c1.name="皮卡丘";
+				Chongwu c2=new Chongwu();
+				c2.address="香港动物园";
+				System.out.println(c2.name);//皮卡丘  
+				System.out.println(c2.address);//香港动物园
+	}
+
+}
